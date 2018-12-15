5764ec72f884c2660c1a48b707f0c0a95768b1f8
Devin Udy <devin.udy@gmail.com>
2018-12-14 17:16:38 -0700

    delete JSON file, added Firebase database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,6 +8,17 @@
 //   * Users from many different machines must be able to view same train times.
 //   * Styling and theme are completely up to you. Get Creative!
 
+//Firebase Database
+var config = {
+    apiKey: "AIzaSyCAEOQLqfveoVVGitiarFJCAwxH9NmqTxE",
+    authDomain: "trainproject-4c54b.firebaseapp.com",
+    databaseURL: "https://trainproject-4c54b.firebaseio.com",
+    projectId: "trainproject-4c54b",
+    storageBucket: "trainproject-4c54b.appspot.com",
+    messagingSenderId: "127536845307"
+};
+firebase.initializeApp(config);
+
 //Moment API
 var tFrequencey = 15;
 var firstTime = '5:00';
@@ -29,3 +40,11 @@ console.log('Minutes till Train: ' + tMinutesTillTrain);
 //Next Train
 var nextTrain = moment().add(tMinutesTillTrain, 'minutes');
 console.log('Arrival Time: ' + moment(nextTrain).format('HH:mm'));
+//Now display next train arrival and minutes away
+$('#next-train').html(nextTrain.format('HH:mm'));
+$('#minutes-away').html(tMinutesTillTrain);
+//Sweet! Now lets add a new train to the list, using the "Add Train" section
+//Site is not auto updating. Need to add a listener somewhere
+//When Firebase is added, information for next train and minutes until next train are not visible on page
+$('#next-train').append();
+$('#minutes-away').append();
